9c18b80ce1b17fc1cc0bcb979b2ba96a16304a77
Akira <akame913@gmail.com>
2014-11-25 18:26:09 +0900

    Finish full title 2nd

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,9 +8,9 @@ describe "Static pages" do
   describe "Home page" do
     before { visit root_path }
 
-    it { should have_content('Home') }
-    #it { should have_title(full_title('Home')) }
-    #it { should_not have_title('| Home') }
+    it { should have_content('Ebara 4th') }
+    it { should have_title(full_title('')) }
+    it { should_not have_title('| Home') }
 
     #describe "for signed-in users" do
     #  let(:user) { FactoryGirl.create(:user) }
@@ -39,14 +39,14 @@ describe "Static pages" do
     before { visit help_path }
 
     it { should have_content('Help') }
-    #it { should have_title(full_title('Help')) }
+    it { should have_title(full_title('Help')) }
   end
 
   describe "About page" do
     before { visit about_path }
 
     it { should have_content('About') }
-    #it { should have_title(full_title('About')) }
+    it { should have_title(full_title('About')) }
   end
 
   #it "should have the right links on the layout" do
