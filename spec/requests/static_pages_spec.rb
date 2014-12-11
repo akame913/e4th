99c44c65463aeed7af4de909e21a60a693a6fb5b
Akira <akame913@gmail.com>
2014-12-11 11:38:57 +0900

    Finish change layout

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,7 +8,7 @@ describe "Static pages" do
   describe "Home page" do
     before { visit root_path }
 
-    it { should have_content('Ebara 4th') }
+    #it { should have_content('Ebara 4th') }
     it { should have_title(full_title('')) }
     it { should_not have_title('| ホーム') }
 
