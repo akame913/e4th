a30bbacf81ce90c2af0f33de1ef46f3070ce99f2
Akira <akame913@gmail.com>
2015-04-02 17:34:19 +0900

    Finish add mailer production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,16 @@ E4th::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+	  address:               'smtp.gmail.com',
+	  port:      	           587,
+	  domain:                'example.com',
+	  user_name:             'akame913@gmail.com',
+	  password:              'kamegmail1020',
+	  authentication:        'plain',
+	  enable_starttls_auto:  true
+  }
+
 end
